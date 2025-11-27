e5b45719b55f5d0c80e37ab9d4178a5b42f51cf3
Alejandro Huguet Lorente <A.HuguetLorente@student.tudelft.nl>
2025-11-27 23:53:12 +0100

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -59,7 +59,7 @@ const VideoPlayer = ({ videoId }) => {
         <iframe 
           width="100%" 
           height="100%" 
-          src={`https://www.youtube.com/embed/ItiWPmPzdi0?autoplay=0`} 
+          src={`https://www.youtube.com/embed/qNjEO-0bE1g?autoplay=0`} 
           title="YouTube video player" 
           frameBorder="0" 
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
